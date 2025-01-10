a82f3368550943900b27ac88adca44be5f8abb1f
thomaslaurenson <thomas@thomaslaurenson.com>
2025-01-10 14:53:54 +1300

    Fixed up index docs

--- dist/config/install.sh
+++ dist/config/install.sh
@@ -8,9 +8,9 @@ URL_TMUXCONF="https://pub.thomaslaurenson.com/config/tmux.conf"
 
 # Determine remote fetch command
 if command -v curl &> /dev/null; then
-    FETCH_CMD="curl -o"
+    FETCH_CMD="curl -s -o"
 elif command -v wget &> /dev/null; then
-    FETCH_CMD="wget -O"
+    FETCH_CMD="wget -q -O"
 else
     echo "[!] No fetch command found... Exiting"
     exit 1
@@ -18,17 +18,19 @@ fi
 
 # Version (set in GitHub Actions deploy.yml workflow)
 PUB_VERSION=""
+echo "[*] Setting pub version: $PUB_VERSION"
 # Overwrite, or add, version in ~/.bashrc
 if grep "# PUB VERSION" ~/.bashrc > /dev/null; then
-    sed -i 's/export PUB_VERSION=v0.1.3
+    sed -i "s/export PUB_VERSION=.*/export PUB_VERSION=$PUB_VERSION/" ~/.bashrc
 else
     {
         echo -e "\n# PUB VERSION"
-        echo -e "export PUB_VERSION=v0.1.3
+        echo -e "export PUB_VERSION=$PUB_VERSION"
     }  >> ~/.bashrc
 fi
 
 # Fetch and add aliases file to ~/.bashrc
+echo "[*] Configuring aliases..."
 $FETCH_CMD "$HOME/.aliases" https://pub.thomaslaurenson.com/config/aliases
 if ! grep "# CUSTOM ALIASES" ~/.bashrc > /dev/null; then
     {
@@ -40,8 +42,10 @@ if ! grep "# CUSTOM ALIASES" ~/.bashrc > /dev/null; then
 fi
 
 # Fetch and add gitignore config
+echo "[*] Configuring git..."
 $FETCH_CMD "$HOME/.gitconfig" https://pub.thomaslaurenson.com/config/gitconfig
 
 # Fetch and add tmux.conf file
+echo "[*] Configuring tmux..."
 $FETCH_CMD "$HOME/.tmux.conf" https://pub.thomaslaurenson.com/config/tmux.conf
 tmux source ~/.tmux.conf
