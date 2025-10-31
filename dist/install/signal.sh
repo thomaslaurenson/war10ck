dfc5ab83de62c2f817c2abaf0a4ade9518a98c15
Thomas Laurenson <thomas@thomaslaurenson.com>
2025-11-01 09:05:30 +1300

    Feature/jira and sshfs (#29)
    * Updated sshfs function which closes #28
    * Fixed jira aliases which closes #27
    * Changed tabs to spaces

--- dist/install/signal.sh
+++ dist/install/signal.sh
@@ -2,11 +2,11 @@
 
 
 sudo wget -O- https://updates.signal.org/desktop/apt/keys.asc \
-	| gpg --dearmor \
-	| sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+    | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 
 sudo wget -O- https://updates.signal.org/static/desktop/apt/signal-desktop.sources \
-	| sudo tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
+    | sudo tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
 
 sudo apt update
 sudo apt install signal-desktop
