99e9df4161f9db4719b9346d7115b8ad8b5fd264
thomaslaurenson <thomas@thomaslaurenson.com>
2026-06-16 18:49:23 +1200

    Bumped version

--- src/lib/version.sh
+++ src/lib/version.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034  # Variables are used across bundled files
 
 # Manually increment VERSION before tagging a new release.
-readonly VERSION="v0.4.14"
+readonly VERSION="v0.5.0"
 
 version() {
   local display="${VERSION}"
