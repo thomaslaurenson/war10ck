0e66ce0872e5870d4bd2c52fe09b803cf379e1b6
thomaslaurenson <thomas@thomaslaurenson.com>
2026-07-01 12:16:08 +1200

    Fixed keyboard shortcuts

--- src/modules/i3/install.sh
+++ src/modules/i3/install.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 [[ "${WAR10CK_DEBUG:-0}" == "1" ]] && set -x
 
-w_apt_install i3 i3lock xss-lock dex network-manager-gnome pulseaudio-utils x11-xserver-utils
+w_apt_install i3 i3lock dex pulseaudio-utils x11-xserver-utils brightnessctl libnotify-bin scrot gnome-keyring network-manager-gnome
 
 w_log_info "i3 module installed."
