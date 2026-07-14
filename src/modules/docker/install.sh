ba9375e90e1da2f455bfdcd00a4453b280b33644
thomaslaurenson <thomas@thomaslaurenson.com>
2026-07-15 11:22:50 +1200

    Make docker module more flexible for linus systems

--- src/modules/docker/install.sh
+++ src/modules/docker/install.sh
@@ -6,9 +6,12 @@ set -euo pipefail
 w_apt_remove docker docker-engine docker.io containerd runc
 
 w_apt_install ca-certificates curl gnupg
-w_apt_add_key "docker" "https://download.docker.com/linux/ubuntu/gpg"
 # shellcheck disable=SC1091
-w_apt_add_source "docker" "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable"
+DISTRO_ID=$(. /etc/os-release && echo "$ID")
+# shellcheck disable=SC1091
+DISTRO_CODENAME=$(. /etc/os-release && echo "$VERSION_CODENAME")
+w_apt_add_key "docker" "https://download.docker.com/linux/${DISTRO_ID}/gpg"
+w_apt_add_source "docker" "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DISTRO_ID} ${DISTRO_CODENAME} stable"
 
 w_apt_install \
     docker-ce \
